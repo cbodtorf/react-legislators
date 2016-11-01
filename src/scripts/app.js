21eb3976fe996e5555b77bf9d95e7d3b4a4c1a39
Cbodtorf <cbodtorf@gmail.com>
2016-11-01 14:44:20 -0400

    added basic react components in both class and constructor style.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,39 @@
-const Backbone = require('Backbone')
+import $ from 'jquery'
+import Backbone from 'backbone'
+import React, { Component, PropTypes } from 'react'
+import ReactDom from 'react-dom'
 
-console.log(Backbone)
-document.querySelector('#app-container').innerHTML = `<h1>YOLO</h1>`
+/**
+* Sunlight Foundation API: https://sunlightfoundation.com/api/
+*/
+
+/**
+* React App ES6 class style
+*/
+
+class AppClass extends Component {
+  render() {
+    return (
+      <div>
+        <h6>Wowzers this is a ES6 Component!</h6>
+      </div>
+    )
+  }
+}
+ReactDom.render(<AppClass />, document.getElementById('appOne'))
+
+
+/**
+* React App constructor function style
+*/
+
+var AppConstructor = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h6>Insane, this is a React Constructor Component </h6>
+      </div>
+    )
+  }
+})
+ReactDom.render(<AppConstructor />, document.getElementById('appTwo'))
